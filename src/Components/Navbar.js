92798282ed0a02b809c75ecbc35427ad4e6a8839
emmanuel <emmanuel.ratier@epitech.eu>
2019-01-28 12:39:29 +0100

    install materializecss

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h4>la nav</h4>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
